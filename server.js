edad4fc112c77bbfc32b0a83267310a8b4b4843d
byarustev <byarus45@gmail.com>
2019-07-24 22:59:14 +0300

    document api endpoints

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 //cross origin
 app.use( (request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*");
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
